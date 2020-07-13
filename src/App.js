cb5636736cdc9d9c5a44611df52bdf729e6b9ee3
Daria <dariadar12@gmail.com>
2020-07-13 14:07:46 +0300

    add font awesome and scss

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import './App.css';
+import './styles/global.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCheckSquare } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faCheckSquare);
 
 function App() {
   return (
     <div className="App">
-      <h1>Hey</h1>
+      <h1>Hey <FontAwesomeIcon icon='check-square' /></h1>
     </div>
   );
 }
